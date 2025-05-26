8d9c6ebe6c23dfc5037f6e71061d0a7e32f6fa66
nhat191024 <richberchannel01@gmail.com>
2025-05-26 17:16:51 +0700

    feat: update DatabaseSeeder to include medical categories, languages, and appointments seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,21 +3,52 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Setting;
+use App\Models\Language;
+use App\Models\Appointment;
+use App\Models\DoctorProfile;
+use App\Models\MedicalCategory;
+
+use App\Models\Bill;
+use App\Models\Review;
+use App\Models\Notification;
+
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Testing\Fakes\Fake;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
+        $jsonFilePath = './database/seeders/data.json';
+        $jsonContent = file_get_contents($jsonFilePath);
+        $dataArray = json_decode($jsonContent, true);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        foreach ($dataArray['medical_categories'] as $data) {
+            MedicalCategory::create($data);
+        }
+
+        foreach ($dataArray['languages'] as $data) {
+            Language::create($data);
+        }
+
+        //user factory
+        User::factory()->count(env('DEMO_DOCTORS'))->doctor()->create();
+        User::factory()->count(env('DEMO_PHARMACISTS'))->pharmacy()->create();
+        User::factory()->count(env('DEMO_HOSPITALS'))->hospital()->create();
+        User::factory()->count(env('DEMO_PATIENTS'))->patient()->create();
+
+        //setting factory
+        Setting::factory()->count(1)->create();
+
+        //appointment factory
+        $doctorsID = DoctorProfile::where('identity', '=', 'doctor')->pluck('user_id')->toArray();
+        $patientsID = User::where('user_type', 'patient')->pluck('id')->toArray();
+        Appointment::factory()->count(10)->withSeedData($doctorsID, $patientsID)->create();
     }
 }
