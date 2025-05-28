dee10f53d0b7b50a9a04d639d975b1552c591eba
nhat191024 <richberchannel01@gmail.com>
2025-05-28 12:26:51 +0700

    feat: remove Wallet trait and interface from User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,9 +12,6 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Filament\Panel;
 use Filament\Models\Contracts\FilamentUser;
 
-use Bavix\Wallet\Traits\HasWallet;
-use Bavix\Wallet\Interfaces\Wallet;
-
 class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable, HasApiTokens, SoftDeletes;
