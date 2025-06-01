a6386b504bf706368cdded135d50d51ac9b27725
nhat191024 <richberchannel01@gmail.com>
2025-06-01 16:46:31 +0700

    feat: add id_card, medical_degree, professional_card, and exploitation_license labels to language files for English and Vietnamese

--- lang/en/doctor.php
+++ lang/en/doctor.php
@@ -3,5 +3,10 @@ return [
     "admin"  => [
         'professional_number' => 'Professional Number',
         'medical_category' => 'Medical Category',
+
+        'id_card' => 'ID Card',
+        'medical_degree' => 'Medical Degree',
+        'professional_card' => 'Professional Card',
+        'exploitation_license' => 'Exploitation License',
     ]
 ];
--- lang/vi/doctor.php
+++ lang/vi/doctor.php
@@ -3,5 +3,10 @@ return [
     "admin"  => [
         'professional_number' => 'Số chứng chỉ',
         'medical_category' => 'Chuyên khoa',
+
+        'id_card' => 'CMND/CCCD',
+        'medical_degree' => 'Bằng cấp y khoa',
+        'professional_card' => 'Thẻ hành nghề',
+        'exploitation_license' => 'Giấy phép hành nghề',
     ]
 ];
