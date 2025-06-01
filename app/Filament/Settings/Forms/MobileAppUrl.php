a55091b88b51ccb9825223abd6ea6cf533239422
nhat191024 <richberchannel01@gmail.com>
2025-06-01 15:28:33 +0700

    refactor: standardize labels in Mobile App URL settings form to use common translation keys

--- app/Filament/Settings/Forms/MobileAppUrl.php
+++ app/Filament/Settings/Forms/MobileAppUrl.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Settings\Forms;
 
 use Filament\Forms\Components\Tabs\Tab;
+use Filament\Forms\Components\TextInput;
 
 class MobileAppUrl
 {
@@ -12,7 +13,7 @@ class MobileAppUrl
     public static function getTab(): Tab
     {
         return Tab::make('mobile_app_url')
-            ->label(__('Mobile App URL'))
+            ->label(__('common.mobile_app_url'))
             ->icon('heroicon-o-computer-desktop')
             ->schema(self::getFields())
             ->columns()
@@ -23,20 +24,20 @@ class MobileAppUrl
     public static function getFields(): array
     {
         return [
-            \Filament\Forms\Components\TextInput::make('app_url')
-                ->label(__('App URL'))
+            TextInput::make('app_url')
+                ->label(__('common.app_url'))
                 ->placeholder('https://example.com')
                 ->maxLength(250)
                 ->required()
                 ->columnSpanFull(),
-            \Filament\Forms\Components\TextInput::make('play_store_url')
-                ->label(__('Play Store URL'))
+            TextInput::make('play_store_url')
+                ->label(__('common.play_store_url'))
                 ->placeholder('https://play.google.com/store/apps/details?id=com.example.app')
                 ->maxLength(250)
                 ->required()
                 ->columnSpanFull(),
-            \Filament\Forms\Components\TextInput::make('app_store_url')
-                ->label(__('App Store URL'))
+            TextInput::make('app_store_url')
+                ->label(__('common.app_store_url'))
                 ->placeholder('https://apps.apple.com/app/id1234567890')
                 ->maxLength(250)
                 ->required()
