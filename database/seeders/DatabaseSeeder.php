d572ea40b9a48011cb34b428231eb840a7d45a2a
nhat191024 <richberchannel01@gmail.com>
2025-06-01 14:38:18 +0700

    refactor: reorganize imports and enhance app settings seeding with additional keys

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,19 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use App\Models\Setting;
-use App\Models\Language;
-use App\Models\Appointment;
-use App\Models\DoctorProfile;
-use App\Models\MedicalCategory;
+use Illuminate\Support\Facades\DB;
 
 use App\Models\Bill;
+use App\Models\User;
 use App\Models\Review;
-use App\Models\Notification;
+use App\Models\Setting;
+use App\Models\Language;
 
+use App\Models\Appointment;
+use App\Models\Notification;
+use App\Models\DoctorProfile;
+
+use App\Models\MedicalCategory;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Testing\Fakes\Fake;
 
@@ -44,12 +46,54 @@ class DatabaseSeeder extends Seeder
         User::factory()->count(env('DEMO_HOSPITALS'))->hospital()->create();
         User::factory()->count(env('DEMO_PATIENTS'))->patient()->create();
 
-        //setting factory
-        Setting::factory()->count(1)->create();
-
         //appointment factory
         $doctorsID = DoctorProfile::whereIn('user_id', User::where('identity', 'doctor')->pluck('id'))->pluck('id')->toArray();
         $patientsID = User::where('user_type', 'patient')->pluck('id')->toArray();
         Appointment::factory()->count(10)->withSeedData($doctorsID, $patientsID)->create();
+
+        DB::table('app_settings')->insert([
+            [
+                'id' => '1',
+                'tab' => 'app',
+                'key' => 'app_logo',
+                'value' => 'assets/site_logo.png',
+            ],
+            [
+                'id' => '2',
+                'tab' => 'app',
+                'key' => 'app_dark_logo',
+                'value' => 'assets/site_dark_logo.png',
+            ],
+            [
+                'id' => '3',
+                'tab' => 'app',
+                'key' => 'app_favicon',
+                'value' => 'assets/site_favicon.ico',
+            ],
+            [
+                'id' => '4',
+                'tab' => 'app',
+                'key' => 'app_name',
+                'value' => 'MedLink',
+            ],
+            [
+                'id' => '5',
+                'tab' => 'app',
+                'key' => 'support_email',
+                'value' => 'medlink@gmail.com',
+            ],
+            [
+                'id' => '6',
+                'tab' => 'app',
+                'key' => 'support_phone_1',
+                'value' => '+84793732392',
+            ],
+            [
+                'id' => '7',
+                'tab' => 'app',
+                'key' => 'support_phone_2',
+                'value' => '+84793732392',
+            ],
+        ]);
     }
 }
