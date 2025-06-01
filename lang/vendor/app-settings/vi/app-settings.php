20fbf97e0b323f42c8819ed58804c04ef36e86bb
nhat191024 <richberchannel01@gmail.com>
2025-06-01 15:32:55 +0700

    feat: add Vietnamese translations for app settings

---
+++ lang/vendor/app-settings/vi/app-settings.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * Copyright CWSPS154. All rights reserved.
+ * @auth CWSPS154
+ * @link  https://github.com/CWSPS154
+ */
+
+return [
+    'system' => 'Hệ thống',
+    'save' => 'Lưu cài đặt',
+    'save-success' => 'Lưu thành công',
+    'form' => [
+        'app' => 'Ứng dụng',
+        'system' => 'Hệ thống',
+        'field' => [
+            'app' => [
+                'name' => 'Tên ứng dụng',
+                'logo' => 'Logo ứng dụng',
+                'dark-logo' => 'Logo giao diện tối',
+                'favicon' => 'Biểu tượng Favicon',
+                'support' => [
+                    'email' => 'Email hỗ trợ',
+                    'phone' => [
+                        '1' => 'Số điện thoại hỗ trợ 1',
+                        '2' => 'Số điện thoại hỗ trợ 2',
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'app' => [
+        'settings' => 'Cài đặt ứng dụng',
+    ],
+];
