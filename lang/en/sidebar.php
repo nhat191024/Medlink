630a53accce3a39d66f38f4c6d09d9d66f041dd7
nhat191024 <richberchannel01@gmail.com>
2025-06-02 15:54:53 +0700

    feat: add medical category translation to sidebar for English and Vietnamese

--- lang/en/sidebar.php
+++ lang/en/sidebar.php
@@ -3,5 +3,6 @@ return [
     "admin"  => [
         "user_management" => "User Management",
         'appointment_management' => 'Appointment Management',
+        'medical_category' => 'Medical Category',
     ]
 ];
