2dd030aad173a1bdda5d79a049d22c2043225465
nhat191024 <richberchannel01@gmail.com>
2025-06-07 22:23:02 +0700

    feat: add password reset request route to authentication API

--- routes/api/auth.php
+++ routes/api/auth.php
@@ -7,3 +7,4 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/register', [LoginController::class, 'register']);
 Route::post('/check-email', [LoginController::class, 'checkEmail']);
+Route::post('/password-reset-request', [LoginController::class, 'passwordResetRequest']);
