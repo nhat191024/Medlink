b408d71172c68f0a729e4f721d77ef0bd59177c2
nhat191024 <richberchannel01@gmail.com>
2025-06-07 22:37:54 +0700

    feat: add auth middleware to password reset request route

--- routes/api/auth.php
+++ routes/api/auth.php
@@ -7,4 +7,4 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/check-email', [AuthController::class, 'checkEmail']);
-Route::post('/password-reset-request', [AuthController::class, 'passwordResetRequest']);
+Route::post('/password-reset-request', [AuthController::class, 'passwordResetRequest'])->middleware('auth:sanctum');
