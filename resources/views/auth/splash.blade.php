9df7cdd8d5a210efc72563298ce564557a8805fc
hoangnam10xhp <hoangnam10xhp@gmail.com>
2025-06-09 01:20:48 +0700

    Complete the login interface

--- resources/views/auth/splash.blade.php
+++ resources/views/auth/splash.blade.php
@@ -3,16 +3,16 @@
 @section('content')
 <div class="login-container">
     <div class="login-left">
-        <img src="/images/image.png" alt="Doctor" class="doctor-img">
+        <img src="/images/happy-black-male-doctor-showing-thumb-up-medical-product-advertising-concept_1262-12343.avif" alt="Doctor" class="doctor-img">
     </div>
     <div class="login-right">
         <div class="login-box">
             <img src="/images/logo.svg" alt="Logo" class="login-logo">
             <h1 class="login-title">Chăm sóc sức khỏe chỉ cần một chút</h1>
-            <a href="{{ route('login') }}" class="logindt-btn">Log In</a>
-            <a href="{{ route('register') }}" class="register-btn">Create an Account</a>
+            <a href="{{ route('login') }}" class="logindt-btn">Đăng nhập</a>
+            <a href="{{ route('register') }}" class="register-btn">Tạo tài khoản</a>
             <div class="divider">
-                <span>or continue with</span>
+                <span>hoặc tiếp tục với</span>
             </div>
             <div class="social-btns">
                 <a href="#"><img src="/images/747.png" alt="Apple"></a>
