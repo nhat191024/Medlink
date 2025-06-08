8fa1f458cf11a1ced23f99ecaf15f310b2ecf127
hoangnam10xhp <hoangnam10xhp@gmail.com>
2025-06-09 01:20:07 +0700

    Create Login Interface

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="main-content">
+    <!-- Chỉ hiển thị navbar, không có nội dung khác -->
+    <!-- Hoặc có thể thêm nội dung trang chủ tùy ý -->
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use League\Csv\Query\Row;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\AuthController;
+
 Route::get('/', function () {
     return view('layouts.app');
 });
 
-Route::get('/login',function(){
-    return view('auth.login');
-});
+// Auth routes
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+    Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
 
-Route::get('/login', [LoginController::class, 'index'])->name('login');
-Route::post('/login', [LoginController::class, 'login']);
+    Route::get('/login', [LoginController::class, 'index'])->name('login');
+    Route::post('/login', [LoginController::class, 'login']);
+});
