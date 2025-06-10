1dd7da17d4410bdf1d35cf6cf9e9ba4baf30dbec
thinh11os <148058969+thinh11os@users.noreply.github.com>
2025-06-10 20:37:35 +0700

    thêm tính năng

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -7,28 +7,26 @@ use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-
     public function index()
     {
         return view('auth.login');
     }
 
+  public function login(Request $request)
+{
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+    ]);
 
-    public function login(Request $request)
-    {
-        $request->validate([
-            'email' => 'required|email',
-            'password' => 'required',
-        ]);
+    $credentials = $request->only('email', 'password');
 
-        $credentials = $request->only('email', 'password');
-
-        if (Auth::attempt($credentials)) {
-            return redirect()->intended('/dashboard');
-        }
-
-        return back()->withErrors([
-            'password' => 'Oops! Password is not correct',
-        ])->withInput($request->only('email'));
+    if (Auth::attempt($credentials)) {
+        return redirect()->intended('/'); 
     }
+
+    return back()->withErrors([
+        'password' => 'Oops! Password is not correct',
+    ])->withInput($request->only('email'));
+}
 }
