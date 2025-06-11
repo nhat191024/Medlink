5f013de9937c0ffb7dc87313539632667d826471
Fumino The Fish <richberchannel01@gmail.com>
2025-06-11 22:34:08 +0700

    feat: add root route to return the main application layout

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,10 @@ use App\Http\Controllers\AuthController;
 
 // Auth routes
 Route::middleware('guest')->group(function () {
+    route::get('/', function () {
+        return view('layouts.app');
+    });
+
     Route::get('/splash', [AuthController::class, 'showSplashForm'])->name('splash');
     Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
     Route::post('/login', [AuthController::class, 'login'])->name('login.post');
