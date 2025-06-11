3b2e44f3517db7024028a8d297dedcbe21cc1316
Fumino The Fish <richberchannel01@gmail.com>
2025-06-11 19:59:44 +0700

    refactor: update login route to use AuthController instead of LoginController

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,6 @@ use App\Http\Controllers\AuthController;
 // Auth routes
 Route::middleware('guest')->group(function () {
     Route::get('/splash', [AuthController::class, 'showSplashForm'])->name('splash');
-    Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
-
-    Route::get('/login', [LoginController::class, 'index'])->name('login');
-    Route::post('/login', [LoginController::class, 'login']);
+    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [AuthController::class, 'login'])->name('login.post');
 });
