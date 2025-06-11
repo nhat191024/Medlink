eff526d5f5918ab9da552d54811176160a7172fd
Fumino The Fish <richberchannel01@gmail.com>
2025-06-11 22:42:50 +0700

    refactor: remove search bar component from navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -44,19 +44,6 @@
             </li>
         </ul>
 
-        <!-- Search Bar -->
-        <div class="navbar-search">
-            <div class="search-container">
-                <input class="search-input" type="text" placeholder="Search by name">
-                <button class="search-button" type="submit">
-                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                        <circle cx="11" cy="11" r="8"></circle>
-                        <path d="m21 21-4.35-4.35"></path>
-                    </svg>
-                </button>
-            </div>
-        </div>
-
         <!-- Auth Buttons -->
         <div class="navbar-auth">
             @guest
