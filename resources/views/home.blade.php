f04cbba379c00b09fa74ce47bbfb8e56c98ac2f3
Fumino The Fish <richberchannel01@gmail.com>
2025-06-11 19:56:57 +0700

    refactor: remove HomeController and home view files

--- resources/views/home.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="main-content">
-    <!-- Chỉ hiển thị navbar, không có nội dung khác -->
-    <!-- Hoặc có thể thêm nội dung trang chủ tùy ý -->
-</div>
-@endsection
