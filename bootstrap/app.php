a2bc0c8f01499e77adf707fc16210c1d89526253
Fumino The Fish <richberchannel01@gmail.com>
2025-06-18 22:40:18 +0700

    feat: add LogRouteAccess middleware to application configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 use App\Http\Middleware\LocalizationMiddleware;
+use App\Http\Middleware\LogRouteAccess;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append(LocalizationMiddleware::class);
+        // $middleware->append(LogRouteAccess::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
