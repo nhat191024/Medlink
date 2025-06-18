6c1e12ffa84049382b404e3db18421e9124d286a
Fumino The Fish <richberchannel01@gmail.com>
2025-06-18 22:41:02 +0700

    feat: add LogRouteAccess middleware for logging route access in local environment

---
+++ app/Http/Middleware/LogRouteAccess.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class LogRouteAccess
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if (env('APP_ENV') === 'local') {
+            $route = $request->route();
+            $routeName = $route?->getName();
+            $routeAction = $route?->getActionName();
+            $url = $request->fullUrl();
+            Log::info('[RouteAccess] Route accessed', [
+                'route_name' => $routeName,
+                'route_action' => $routeAction,
+                'url' => $url,
+                'method' => $request->method(),
+                'ip' => $request->ip(),
+            ]);
+        }
+        return $next($request);
+    }
+}
