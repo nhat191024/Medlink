ef424a0f55312bfc9007faeaa10ba0da036caf18
Fumino The Fish <richberchannel01@gmail.com>
2025-06-19 23:16:58 +0700

    fix: uncomment vendor and storage permissions in entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,12 @@ mkdir -p /var/www/html/storage/framework/{sessions,views,cache}
 chmod -R 775 /var/www/html
 chown -R www-data:www-data /var/www/html
 
+chmod -R 775 /var/www/html/storage
+chown -R www-data:www-data /var/www/html/storage
+
+chmod -R 775 /var/www/html/vendor
+chown -R www-data:www-data /var/www/html/vendor
+
 # npm install
 # npm run build
 
