60ad0bac39dff0947f508ee00059d83a8729c8dd
Fumino The Fish <richberchannel01@gmail.com>
2025-06-22 21:44:58 +0700

    feat: add ServiceCollection and ServiceResource for managing service data

---
+++ app/Http/Resources/ServiceResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ServiceResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            "id" => $this->id,
+            "icon" => $this->icon,
+            "name" => $this->name,
+            "description" => $this->description,
+            "price" => $this->price,
+            "duration" => $this->duration,
+            "buffer_time" => $this->buffer_time,
+            "seat" => $this->seat,
+            "is_active" => $this->is_active,
+            // "doctor" => new UserResource($this->doctor),
+        ];
+    }
+}
