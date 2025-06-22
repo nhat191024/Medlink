07ce55228d030bf1e1ee0402043073aab339fc9b
Fumino The Fish <richberchannel01@gmail.com>
2025-06-22 22:41:35 +0700

    feat: add DoctorAppointmentResource for transforming appointment data

---
+++ app/Http/Resources/DoctorAppointmentResource.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class DoctorAppointmentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            "id" => $this->id,
+            "status" => $this->status,
+            "medical_problem" => $this->medical_problem,
+            "medical_problem_file" => $this->medical_problem_file,
+            "duration" => $this->duration,
+            "date" => $this->date,
+            "day_of_week" => $this->day_of_week,
+            "time" => $this->time,
+            "reason" => $this->reason,
+            "link" => $this->link,
+            "address" => $this->address,
+            "created_at" => $this->created_at,
+            "office_address" => $this->doctor->office_address,
+            "medical_category" => new MedicalCategoryResource($this->whenLoaded('medical_category')),
+            "patient" => new PatientResource($this->whenLoaded('patient')),
+            "service" => new ServiceResource($this->whenLoaded('service')),
+            "bill" => new BillResource($this->whenLoaded('bill')),
+        ];
+    }
+}
