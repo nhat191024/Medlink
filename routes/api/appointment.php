20bb62e40b94239abd095debcd5ef7fdc23e227a
Fumino The Fish <richberchannel01@gmail.com>
2025-06-22 23:35:53 +0700

    fix: update comments for appointment routes to indicate testing status

--- routes/api/appointment.php
+++ routes/api/appointment.php
@@ -4,6 +4,6 @@ use App\Http\Controllers\Api\AppointmentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/appointments/doctor', [AppointmentController::class, 'doctorAppointments']);
-Route::get('/appointments/patient', [AppointmentController::class, 'patientAppointments']);
-Route::post('/appointments/status', [AppointmentController::class, 'updateAppointmentStatus']);
+Route::get('/appointments/doctor', [AppointmentController::class, 'doctorAppointments']); //* Tested
+Route::get('/appointments/patient', [AppointmentController::class, 'patientAppointments']); //! not tested
+Route::post('/appointments/status', [AppointmentController::class, 'updateAppointmentStatus']); //! not tested
