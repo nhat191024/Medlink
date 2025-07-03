e9c7e9b28b722806cbda1d6664c61bbf63a216e4
Fumino The Fish <richberchannel01@gmail.com>
2025-07-03 15:06:36 +0700

    feat: move forgot password routes to a separate file for better organization

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,5 @@ Route::middleware('guest')->group(function () {
     Route::post('/login', [AuthController::class, 'login'])->name('login.post');
 
     require __DIR__ . '/web/register.php';
-
-    Route::get('/forgot-password', [AuthController::class, 'showForgotPasswordForm'])->name('forgot-password');
-    Route::post('/forgot-password', [AuthController::class, 'sendOtp'])->name('forgot-password.send-otp');
-    Route::get('/forgot-password/otp', function () {
-        return view('auth.forgot-password-otp');
-    })->name('forgot-password.otp.form');
-    Route::post('/forgot-password/otp', [AuthController::class, 'verifyOtp'])->name('forgot-password.verify-otp');
-    Route::get('/reset-password', [AuthController::class, 'showResetForm'])->name('reset-password.form');
-    Route::post('/reset-password', [AuthController::class, 'resetPassword'])->name('reset-password.post');
+    require __DIR__ . '/web/forgot-password.php';
 });
