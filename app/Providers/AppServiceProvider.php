4aa1a56c4b6abf2a98c323457ff8ab79422076a3
Fumino The Fish <richberchannel01@gmail.com>
2025-07-07 16:47:35 +0700

    fix: ensure URL scheme is forced to HTTPS in production environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 use BezhanSalleh\FilamentLanguageSwitch\LanguageSwitch;
@@ -25,5 +26,9 @@ class AppServiceProvider extends ServiceProvider
             $switch
                 ->locales(['vi', 'en']); // also accepts a closure
         });
+
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
