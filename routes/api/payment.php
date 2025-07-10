69c402947261544e4b3551946d451bdb8abfec90
Fumino The Fish <richberchannel01@gmail.com>
2025-07-10 14:29:12 +0700

    feat: add change payment status route to payment API

--- routes/api/payment.php
+++ routes/api/payment.php
@@ -6,3 +6,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PaymentController;
 
 Route::get('/payment/methods', [PaymentController::class, 'getPaymentMethods']);
+Route::get('/payment/change-status/{id}/{status}', [PaymentController::class, 'changePaymentStatus']);
