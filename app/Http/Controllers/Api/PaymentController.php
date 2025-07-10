7453f8c0185781085672e3fbdfb81814e006cb3d
Fumino The Fish <richberchannel01@gmail.com>
2025-07-10 14:29:22 +0700

    feat: implement change payment status functionality in PaymentController

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\Api;
 
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use App\Models\Bill;
+use App\Models\Appointment;
 
+use Illuminate\Http\Request;
+
+use App\Http\Controllers\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
 class PaymentController extends Controller
@@ -26,5 +29,35 @@ class PaymentController extends Controller
             ],
         ];
         return response()->json(['methods' => $data], Response::HTTP_OK);
+    }
+
+    public function changePaymentStatus(Request $request)
+    {
+        $bill = Bill::findOrFail($request->input('bill_id'));
+        $status = $request->input('status');
+
+        $bill->status = $status == 'PAID' ? 'paid' : 'unpaid';
+        $bill->save();
+
+        $appointment = Appointment::where('id', $bill->appointment_id)->load('doctor', 'doctor.user', 'doctor.services', 'doctor.medicalCategory')->first();
+
+        $appointmentData = [
+            'id' => $appointment->id,
+            'doctor' => [
+                'id' => $appointment->doctor->id,
+                'name' => $appointment->doctor->user->name,
+                'avatar' => $appointment->doctor->user->avatar,
+                'specialization' => $appointment->doctor->medicalCategory->name,
+            ],
+            'service' => [
+                'icon' => $appointment->service->icon,
+                'name' => $appointment->service->name,
+            ],
+            'time' => `{$appointment->day_of_week}, {$appointment->date}, {$appointment->time}`,
+            'appointmentPlace' => $appointment->link ?? $appointment->address,
+            'status' => $appointment->status,
+        ];
+
+        return response()->json(['message' => 'Payment status changed successfully', $appointmentData], Response::HTTP_OK);
     }
 }
