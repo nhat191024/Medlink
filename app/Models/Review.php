3493bf35c77bfee93f53f46f5b4d49d74ab7cd4c
Fumino The Fish <richberchannel01@gmail.com>
2025-07-14 19:52:02 +0700

    fix: correct relationship methods in Review model for patient and doctor associations

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * 
+ *
  *
  * @property int $id
  * @property int $doctor_profile_id
@@ -50,12 +50,12 @@ class Review extends Model
 
     public function patient()
     {
-        return $this->belongsTo(DoctorProfile::class, 'doctor_profile_id');
+        return $this->belongsTo(DoctorProfile::class, 'patient_profile_id');
     }
 
     public function doctor()
     {
-        return $this->belongsTo(PatientProfile::class, 'patient_profile_id');
+        return $this->belongsTo(PatientProfile::class, 'doctor_profile_id');
     }
 
     public function appointment()
