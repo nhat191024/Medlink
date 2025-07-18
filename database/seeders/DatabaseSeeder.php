7a412ec186de0f986b74b2bd2e3ecc3fe035a4e5
Fumino The Fish <richberchannel01@gmail.com>
2025-07-18 19:20:50 +0700

    fix: generate user avatars based on the first letter of their names

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -47,6 +47,14 @@ class DatabaseSeeder extends Seeder
         User::factory()->count(5)->hospital()->create();
         User::factory()->count(20)->patient()->create();
 
+        $users = User::all();
+        foreach ($users as $user) {
+            $username = $user->name;
+            $usernameFirstLetter = mb_substr($username, 0, 1);
+            $user->avatar = "https://ui-avatars.com/api/?name=" . urlencode($usernameFirstLetter) . "&background=random&size=512";
+            $user->save();
+        }
+
         //appointment factory
         $doctorsID = DoctorProfile::whereIn('user_id', User::where('identity', 'doctor')->pluck('id'))->pluck('id')->toArray();
         $patientsID = PatientProfile::whereIn('user_id', User::where('user_type', '=', 'patient')->pluck('id'))->pluck('id')->toArray();
