ddc715468ab868ac3564a80a1306a1cdca31b9b1
Fumino The Fish <richberchannel01@gmail.com>
2025-07-21 13:10:45 +0700

    feat: add routes for wallet recharge QR and recharge functionality

--- routes/api/wallet.php
+++ routes/api/wallet.php
@@ -7,3 +7,5 @@ use App\Http\Controllers\Api\WalletController;
 
 Route::get('/wallet/balance', [WalletController::class, 'getWalletBalance']);
 Route::get('/wallet/transactions', [WalletController::class, 'getTransactionHistory']);
+Route::post('/wallet/recharge-qr', [WalletController::class, 'getRechargeQRCode']);
+Route::post('/wallet/recharge', [WalletController::class, 'rechargeWallet']);
