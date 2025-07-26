aaffb5dc7a81ccf5113a8847f8042cbdedbf1437
Fumino The Fish <richberchannel01@gmail.com>
2025-07-27 00:10:05 +0700

    feat: schedule appointment status check command to run every minute

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Schedule the appointment status check command to run every minute
+Schedule::command('appointments:check-status')
+    ->everyMinute()
+    ->withoutOverlapping()
+    ->onOneServer();
