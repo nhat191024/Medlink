c65974fb848ca062039c2a750341eaa10b392687
Fumino The Fish <richberchannel01@gmail.com>
2025-08-01 14:08:51 +0700

    feat: add ActivityResource and related pages for activity management

---
+++ app/Filament/Admin/Resources/ActivityResource.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Filament\Admin\Resources;
+
+use Spatie\Activitylog\Models\Activity;
+
+use Filament\Tables\Table;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Filters\TrashedFilter;
+
+use Filament\Tables\Columns\TextColumn;
+
+use Filament\Tables\Actions\Action;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\RestoreAction;
+use Filament\Tables\Actions\BulkActionGroup;
+use Filament\Tables\Actions\DeleteBulkAction;
+use Filament\Tables\Actions\RestoreBulkAction;
+use Filament\Tables\Actions\ForceDeleteAction;
+use Filament\Tables\Actions\ForceDeleteBulkAction;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\DatePicker;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+use App\Filament\Resources\HospitalResource\RelationManagers;
+use App\Filament\Admin\Resources\ActivityResource\Pages\EditActivity;
+use App\Filament\Admin\Resources\ActivityResource\Pages\ListActivities;
+use App\Filament\Admin\Resources\ActivityResource\Pages\CreateActivity;
+use App\Models\Admin;
+
+class ActivityResource extends Resource
+{
+    protected static ?string $model = Activity::class;
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?int $navigationSort = 2;
+
+    public static function getNavigationGroup(): string
+    {
+        return __('common.system');
+    }
+
+    public static function getPluralModelLabel(): string
+    {
+        return __('common.admin.activity');
+    }
+
+    public static function getModelLabel(): string
+    {
+        return __('common.admin.activity');
+    }
+
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->query(Activity::with(['subject', 'causer']))
+            ->columns([
+                TextColumn::make('id')
+                    ->searchable()
+                    ->sortable()
+                    ->label(__('common.admin.id')),
+                TextColumn::make('description')
+                    ->searchable()
+                    ->sortable()
+                    ->label(__('common.admin.action')),
+                TextColumn::make('subject_type')
+                    ->searchable()
+                    ->sortable()
+                    ->label(__('common.admin.subject')),
+                TextColumn::make('subject_id')
+                    ->label(__('common.admin.subject_id')),
+                TextColumn::make('subject_name')
+                    ->searchable()
+                    ->sortable()
+                    ->label(__('common.admin.subject_name'))
+                    ->getStateUsing(function ($record) {
+                        if (!$record->subject_id) {
+                            return 'N/A';
+                        }
+                        return $record->subject?->name ?? 'N/A';
+                    }),
+                TextColumn::make('causer_type')
+                    ->searchable()
+                    ->sortable()
+                    ->label(__('common.admin.causer')),
+                TextColumn::make('causer_id')
+                    ->label(__('common.admin.causer_id')),
+                TextColumn::make('causer_name')
+                    ->searchable()
+                    ->sortable()
+                    ->label(__('common.admin.causer_name'))
+                    ->getStateUsing(function ($record) {
+                        if (!$record->causer_id) {
+                            return 'N/A';
+                        }
+                        return $record->causer?->name ?? 'N/A';
+                    }),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([])
+            ->bulkActions([
+                //
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListActivities::route('/'),
+        ];
+    }
+}
