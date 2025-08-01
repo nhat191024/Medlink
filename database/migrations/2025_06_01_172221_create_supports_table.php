31f0cd97204c0c9ea26c45bea2ddb63c5014b8ba
Fumino The Fish <richberchannel01@gmail.com>
2025-08-02 00:09:49 +0700

    feat: update supports table to use patient_id and doctor_id instead of user_id

--- database/migrations/2025_06_01_172221_create_supports_table.php
+++ database/migrations/2025_06_01_172221_create_supports_table.php
@@ -15,13 +15,15 @@ class CreateSupportsTable extends Migration
     {
         Schema::create('supports', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('patient_id')->nullable();
+            $table->unsignedBigInteger('doctor_id')->nullable();
             $table->unsignedBigInteger('appointment_id')->nullable();
             $table->text('message');
             $table->enum('status', ['open', 'closed'])->default('open');
             $table->timestamps();
 
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('patient_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('doctor_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('appointment_id')->references('id')->on('appointments')->onDelete('set null')->onUpdate('cascade');
         });
     }
