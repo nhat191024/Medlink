ded57fe38e32a62d7045807d42c23afaf0260041
Fumino The Fish <richberchannel01@gmail.com>
2025-08-02 00:58:41 +0700

    feat: update support language files to replace user field with patient and doctor fields

--- lang/en/support.php
+++ lang/en/support.php
@@ -3,11 +3,19 @@ return [
     'support' => 'Support',
 
     'fields' => [
-        'user' => 'User',
+        'patient' => 'Patient',
+        'doctor' => 'Doctor',
+        'appointment_id' => 'Appointment ID',
         'message' => 'Message',
         'status' => 'Status',
         'created_at' => 'Created At',
         'updated_at' => 'Updated At',
+    ],
+
+    'buttons' => [
+        'message' => 'Message',
+        'mark_as_open' => 'Open support request',
+        'mark_as_closed' => 'Close support request',
     ],
 
     'status' => [
