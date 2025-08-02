07d5bb46a229047753dcbcde02a245d6a7890c34
Fumino The Fish <richberchannel01@gmail.com>
2025-08-02 18:15:51 +0700

    feat: add Vietnamese language support for admin fields and placeholders

---
+++ lang/en/admin.php
@@ -0,0 +1,23 @@
+<?php
+return [
+    'fields' => [
+        'name' => 'Name',
+        'email' => 'Email',
+        'password' => 'Password',
+        'role' => 'Role',
+        'avatar' => 'Avatar',
+    ],
+
+    'placeholders' => [
+        'name' => 'Enter admin name',
+        'email' => 'Enter admin email',
+        'password' => 'Enter admin password',
+        'avatar' => 'Enter avatar URL',
+    ],
+
+    'roles' => [
+        'admin' => 'Administrator',
+        'hr' => 'Human Resources',
+        'supervisor' => 'Supervisor',
+    ],
+];
