bc6c45690be155b0ae75ff60ed303e915230db6f
Fumino The Fish <richberchannel01@gmail.com>
2025-08-02 17:01:03 +0700

    feat: add AppointmentResource with CRUD pages for managing appointments

---
+++ app/Filament/Admin/Resources/AppointmentResource/Pages/EditAppointment.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Filament\Admin\Resources\AppointmentResource\Pages;
+
+use App\Filament\Admin\Resources\AppointmentResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditAppointment extends EditRecord
+{
+    protected static string $resource = AppointmentResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+            Actions\ForceDeleteAction::make(),
+            Actions\RestoreAction::make(),
+        ];
+    }
+}
