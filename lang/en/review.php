2cc9f8fe5ba389fde761aa3e4f3588233bd612d2
Fumino The Fish <richberchannel01@gmail.com>
2025-08-03 22:34:45 +0700

    feat: add language files for reviews in English and Vietnamese

---
+++ lang/en/review.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'review' => 'Review',
+
+    'fields' => [
+        'doctor' => 'Doctor name',
+        'patient' => 'Patient name',
+        'appointment' => 'Appointment ID',
+        'rate' => 'Rate',
+        'recommend' => 'Is recommend',
+        'created_at' => 'Created At',
+        'updated_at' => 'Updated At',
+    ],
+];
---
+++ lang/vi/review.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'review' => 'Đánh giá',
+
+    'fields' => [
+        'doctor' => 'Tên bác sĩ',
+        'patient' => 'Tên bệnh nhân',
+        'appointment' => 'ID cuộc hẹn',
+        'rate' => 'Đánh giá',
+        'recommend' => 'Có khuyến nghị',
+        'created_at' => 'Ngày tạo',
+        'updated_at' => 'Cập nhật lần cuối',
+    ],
+];
