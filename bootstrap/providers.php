c5e1f510850bb7069f8ccc9f80541efe65e50a65
Fumino The Fish <richberchannel01@gmail.com>
2025-08-06 14:44:28 +0700

    feat: add FilamentNotificationServiceProvider and HrPanelProvider to providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,8 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\FilamentNotificationServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\Filament\HospitalPanelProvider::class,
+    App\Providers\Filament\HrPanelProvider::class,
 ];
