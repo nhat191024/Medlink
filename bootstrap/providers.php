13e9b9fa604b47f5949852cbdeae6c89f487664b
Fumino The Fish <richberchannel01@gmail.com>
2025-08-06 17:44:39 +0700

    feat: add DoctorPanelProvider to the list of service providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,6 +4,7 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\FilamentNotificationServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\Filament\DoctorPanelProvider::class,
     App\Providers\Filament\HospitalPanelProvider::class,
     App\Providers\Filament\HrPanelProvider::class,
 ];
