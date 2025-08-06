f5fca2dfef993c84f6a89544c14f6294689ae2e2
Fumino The Fish <richberchannel01@gmail.com>
2025-08-06 21:41:45 +0700

    feat: add English and Vietnamese language files for service management

---
+++ lang/en/service.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'service' => 'Service',
+
+    'fields' => [
+        'name' => 'Name',
+        'description' => 'Description',
+        'price' => 'Price',
+        'duration' => 'Duration',
+        'is_active' => 'Is Active',
+    ],
+
+    'created_at' => 'Created At',
+    'updated_at' => 'Updated At',
+];
