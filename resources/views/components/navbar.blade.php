c7ec34eaca8944080e4f619379aceb9bcb65969c
Fumino The Fish <richberchannel01@gmail.com>
2025-08-10 17:17:21 +0700

    feat: enhance auth section with user avatar and username display

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -49,17 +49,31 @@
             </li>
         </ul>
 
+        <?php
+        $user = auth()->user();
+        $avatar = $user->avatar ?? '';
+        $username = $user->name ?? '';
+        ?>
+
         <!-- Auth Buttons -->
         <div class="navbar-auth">
             @guest
                 <a class="auth-button" href="{{ route('splash') }}">{{ __('client/navbar.button.login') }} /
                     {{ __('client/navbar.button.register') }}</a>
             @else
-                <!-- Hiển thị khi đã đăng nhập -->
-                <form class="auth-form" method="POST" action="{{ route('logout') }}">
-                    @csrf
-                    <button class="auth-button" type="submit">{{ __('client/navbar.button.logout') }}</button>
-                </form>
+                <div class="flex items-center gap-2">
+                    <a class="avatar" href="{{ route('profile') }}">
+                        <div class="w-12 rounded-full">
+                            <img src="{{ asset($avatar) }}" />
+                        </div>
+                    </a>
+                    <span>{{ $username }} </span>
+                    <!-- Hiển thị khi đã đăng nhập -->
+                    <form class="auth-form" method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <button class="auth-button" type="submit"><x-tabler-logout /></button>
+                    </form>
+                </div>
             @endguest
         </div>
     </div>
