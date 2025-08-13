7ad8f3523a69a014058621863b9810faad60884c
Fumino The Fish <richberchannel01@gmail.com>
2025-08-13 19:06:51 +0700

    feat: add app-no-layout blade template for consistent layout structure

---
+++ resources/views/layouts/app-no-layout.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Medlink') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    @stack('styles')
+
+    <!-- Alpine.js -->
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    <!-- Main Content -->
+    <main>
+        @if (session('error'))
+            <div class="alert alert-error m-3">
+                <span>{{ session('error') }}</span>
+            </div>
+        @endif
+        @if (session('success'))
+            <div class="alert alert-success m-3">
+                <span>{{ session('success') }}</span>
+            </div>
+        @endif
+
+        @yield('content')
+
+    </main>
+
+    <!-- Scripts -->
+    @stack('scripts')
+</body>
+
+</html>
