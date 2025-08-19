479fe8556759b822419599631ca8a1c607cd5fe9
Fumino The Fish <richberchannel01@gmail.com>
2025-08-20 03:41:48 +0700

    feat: add appointment history link to user profile

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -350,6 +350,10 @@
                                 <x-heroicon-s-calendar-days class="h-5 w-5" />
                                 Đặt lịch hẹn
                             </a>
+                            <a class="flex w-full items-center justify-center gap-2 rounded-xl bg-gradient-to-r from-blue-500 to-blue-600 px-4 py-3 font-semibold text-white transition-all duration-300 hover:from-blue-600 hover:to-blue-700" href="{{ route('profile.appointment-history') }}">
+                                <x-heroicon-s-clipboard-document-list class="h-5 w-5" />
+                                Lịch sử đặt khám
+                            </a>
                             <a class="flex w-full items-center justify-center gap-2 rounded-xl bg-gradient-to-r from-green-500 to-green-600 px-4 py-3 font-semibold text-white transition-all duration-300 hover:from-green-600 hover:to-green-700" href="{{ route('profile.edit') }}">
                                 <x-heroicon-s-cog-6-tooth class="h-5 w-5" />
                                 Chỉnh sửa hồ sơ
