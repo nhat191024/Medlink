34d24bb1f62efc9852c23be835cc5426dc7e623b
Fumino The Fish <richberchannel01@gmail.com>
2025-08-20 03:42:02 +0700

    feat: update profile routes to include appointment history

--- routes/web/profile.php
+++ routes/web/profile.php
@@ -3,8 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PatientProfileController;
 
-Route::prefix('forgot-password')->name('profile.')->group(fn() => [
-    Route::get('/profile', [PatientProfileController::class, 'show'])->name('index'),
-    Route::get('/profile/edit', [PatientProfileController::class, 'edit'])->name('edit'),
-    Route::put('/profile', [PatientProfileController::class, 'update'])->name('update')
-]);
+Route::prefix('profile')->name('profile.')->group(function () {
+    Route::get('/', [PatientProfileController::class, 'show'])->name('index');
+    Route::get('/edit', [PatientProfileController::class, 'edit'])->name('edit');
+    Route::put('/', [PatientProfileController::class, 'update'])->name('update');
+    Route::get('/appointment-history', [PatientProfileController::class, 'appointmentHistory'])->name('appointment-history');
+});
