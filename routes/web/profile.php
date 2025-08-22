a3147c4fcd39148e1d6a8fb1c4c3ec25325cc908
Fumino The Fish <richberchannel01@gmail.com>
2025-08-22 23:02:58 +0700

    feat: add appointment review route to profile

--- routes/web/profile.php
+++ routes/web/profile.php
@@ -9,3 +9,6 @@ Route::prefix('profile')->name('profile.')->group(function () {
     Route::put('/', [PatientProfileController::class, 'update'])->name('update');
     Route::get('/appointment-history', [PatientProfileController::class, 'appointmentHistory'])->name('appointment-history');
 });
+
+// Appointment review route
+Route::post('/appointment/{appointment}/review', [PatientProfileController::class, 'submitReview'])->name('appointment.review');
