0ebe6a9bdad1d4bae0cd021c3dc85b54570d9e4e
Fumino The Fish <richberchannel01@gmail.com>
2025-09-01 16:31:07 +0700

    fix: update navbar and home links to point to medical specialties route

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -87,7 +87,7 @@
         <ul class="menu menu-horizontal px-1">
             <li><a href="/">{{ __('client/navbar.menu.home') }}</a></li>
             <li><a href="{{ route('medical-specialties.index') }}">{{ __('client/navbar.menu.medical_category') }}</a></li>
-            <li><a href="{{ route('appointment.index') }}">{{ __('client/navbar.menu.doctor_consultation') }}</a></li>
+            <li><a href="{{ route('medical-specialties.index') }}">{{ __('client/navbar.menu.doctor_consultation') }}</a></li>
             <li><a href="/">{{ __('client/navbar.menu.support') }}</a></li>
         </ul>
     </div>
